fc8a94bd7078b2d267d55c88d2af3ecbd9a54d69
whitedark <whitedarkdmorningstar@gmail.com>
2026-07-14 20:35:03 +0630

    mbs

--- src/app/edd/formula.ts
+++ src/app/edd/formula.ts
@@ -11,19 +11,19 @@ const MONTHS = [
   "October",
   "November",
   "December",
-]
+];
 
 // Calculate EDD by LMP
 export function calculateEDDByLMP(day: number, month: number, year: number) {
-  const lmp = new Date(year, month, day).getTime()
+  const lmp = new Date(year, month - 1, day).getTime();
 
   // Add 40 weeks to lmp
-  const duration = 40 * 7 * 24 * 60 * 60 * 1000
+  const duration = 40 * 7 * 24 * 60 * 60 * 1000;
 
-  const edd = new Date(lmp + duration)
-  const dd = edd.getDate().toString().padStart(2, "0")
-  const mm = (edd.getMonth() + 1).toString().padStart(2, "0")
-  const yyyy = edd.getFullYear().toString()
+  const edd = new Date(lmp + duration);
+  const dd = edd.getDate().toString().padStart(2, "0");
+  const mm = (edd.getMonth() + 1).toString().padStart(2, "0");
+  const yyyy = edd.getFullYear().toString();
 
-  return `${dd}.${mm}.${yyyy} or ${MONTHS[edd.getMonth()]} ${dd}, ${yyyy}`
+  return `${dd}.${mm}.${yyyy} or ${MONTHS[edd.getMonth()]} ${dd}, ${yyyy}`;
 }
